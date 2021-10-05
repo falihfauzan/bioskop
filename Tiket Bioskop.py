0e87c135bc1a7b2b15001ae425044e6f1963531b
falihfauzan <90824329+falihfauzan@users.noreply.github.com>
2021-10-05 10:36:13 +0700

    Push ke 3

--- Tiket Bioskop.py
+++ Tiket Bioskop.py
@@ -1,8 +1,8 @@
 print ("**********Selamat Datang Di CGF Blitch**********")
 
-print ("\nDaftar Film Hari ini :")
+print ("Daftar Film Hari ini :")
 def list ():
-    print ("==========================================")
+    print ("=======================================================")
     print ("(1) **Teater 1**")
     print ('- Film 1                    : 10.00     = Rp. 50.000')
     print ('- Film 2                    : 12.00     = Rp. 50.000')
@@ -11,81 +11,96 @@ def list ():
     print ('- Film 4                    : 11.00     = Rp. 50.000')
     print ('- Film 5                    : 13.00     = Rp. 50.000')
     print ('- Film 6                    : 15.00     = Rp. 50.000')
-    print ("==========================================")
+    print ("=======================================================")
 list()
 
-a = int (input("Silahkan Pilih Teater (Silahan Ketik Nomor)"))
-if a == 1:
+nama = input ("Silahkan Input Nama : ")
+teater = int (input("Silahkan Pilih Teater (Silahan Ketik Nomor)"))
+if teater == 1:
     print ("1. Film 1      : 10.00")
     print ("2. Film 2      : 12.00")
     print ("3. Film 3      : 14.00")
-elif a == 2:
+elif teater == 2:
     print ("4. Film 4      : 11.00")
     print ("5. Film 5      : 13.00")
     print ("6. Film 6      : 15.00")
 else:
     while True:
         print ("**********Teater Tidak Tersedia, Silahkan Pilih Lagi!**********")
-        a = int (input("Silahkan Pilih Teater (Silahkan Ketik Nomor): "))
-        if a == 1 or a == 2:
-            if a == 1:
+        teater = int (input("Silahkan Pilih Teater (Silahkan Ketik Nomor): "))
+        if teater == 1 or teater == 2:
+            if teater == 1:
                 print ("1. Film 1      : 10.00")
                 print ("2. Film 2      : 12.00")
                 print ("3. Film 3      : 14.00")
-            elif a == 2:
+            elif teater == 2:
                 print ("4. Film 4      : 11.00")
                 print ("5. Film 5      : 13.00")
                 print ("6. Film 6      : 15.00")
             break
-a = int (input("Silahkan Pilih Film (Silahkan Ketik Nomor)"))
-if a == 1:
-    spec = 'Film 1 (10.00)'
+film = int (input("Silahkan Pilih Film (Silahkan Ketik Nomor)"))
+if film == 1:
+    spec = 'Film 1'
+    jam = '10:00'
     harga = 50000
-elif a == 2:
-    spec = 'Film 2 (12.00)'
+elif film == 2:
+    spec = 'Film 2'
+    jam = '12:00'
     harga = 50000
-elif a == 3:
-    spec = 'Film 3 (14.00)'
+elif film == 3:
+    spec = 'Film 3'
+    jam = '14:00'
     harga = 50000
-elif a == 4:
-    spec = 'Film 4 (11.00)'
+elif film == 4:
+    spec = 'Film 4'
+    jam = '11:00'
     harga = 50000
-elif a == 5:
-    spec = 'Film 5 (13.00)'
+elif film == 5:
+    spec = 'Film 5'
+    jam = '13:00'
     harga = 50000
-elif a == 6:
-    spec = 'Film 6 (15.00)'
+elif film == 6:
+    spec = 'Film 6'
+    jam = '15:00'
     harga = 50000
 else:
     while True:
         print ("**********Film Tidak Tersedia, Silahkan Pilih Lagi!**********")
-        a = int (input("Silahkan Pilih Film(Silahkan Ketik Nomor): "))
-        if a == 1 or a == 2 or a == 3 or a == 4 or a == 5 or 6:
-            if a == 1:
-                spec = 'Film 4 (11.00)'
+        film = int (input("Silahkan Pilih Film(Silahkan Ketik Nomor): "))
+        if film == 1 or film == 2 or film == 3 or film == 4 or film == 5 or 6:
+            if film == 1:
+                spec = 'Film 1'
+                jam = '10:00'
                 harga = 50000
-            elif a == 2:
-                spec = 'Film 5 (13.00)'
+            elif film == 2:
+                spec = 'Film 2'
+                jam = '12:00'
                 harga = 50000
-            elif a == 3:
-                spec = 'Film 6 (15.00)'
+            elif film == 3:
+                spec = 'Film 3'
+                jam = '14:00'
                 harga = 50000
-            elif a == 4:
-                spec = 'Film 4 (11.00)'
+            elif film == 4:
+                spec = 'Film 4'
+                jam = '11:00'
                 harga = 50000
-            elif a == 5:
-                spec = 'Film 5 (13.00)'
+            elif film == 5:
+                spec = 'Film 5'
+                jam = '13:00'
                 harga = 50000
-            elif a == 6:
-                spec = 'Film 6 (15.00)'
+            elif film == 6:
+                spec = 'Film 6'
+                jam = '15:00'
                 harga = 50000
             break
-total = int (input("Masukan Jumlah Tiket Pembelian :"))
-print('\nNota Pembelian Tiket')
-
-print('Film          :',spec)
-print('Jumlah Tiket     :',total)
-print('Harga            :Rp.',harga)
+total = int (input("Masukkan Jumlah Tiket Pembelian :"))
+print("\nNota Pembelian Tiket")
+print("```````````````````````")
+print ("Nama          :" +str(nama))
+print('Film          :'+str(spec))
+print('Pukul         :' +str(jam))
+print('Jumlah Tiket  :',total)
+print('Harga         :Rp.',harga)
 bayar = total * harga
 
 print ("Anda Harus Membayar : Rp.{}".format(bayar))
@@ -96,7 +111,7 @@ print('\nNote : Bonus Didalam Teater')
 bonus = ['botol Air Minum','Pop Corn']
 for i in range (len(bonus)):
     print("1",bonus[i])
-print ("===========================================================================")
+print ("===================================================")
 print ('******** Terima Kasih Dan Selamat Menonton ********')
-print ("===========================================================================")
+print ("===================================================")
 exit()
