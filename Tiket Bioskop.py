f4171dcd781d4c8fec94c49215ddad5ce6a3d950
falihfauzan <90824329+falihfauzan@users.noreply.github.com>
2021-11-16 09:06:17 +0700

    Push 4

--- Tiket Bioskop.py
+++ Tiket Bioskop.py
@@ -20,10 +20,72 @@ if teater == 1:
     print ("1. Film 1      : 10.00")
     print ("2. Film 2      : 12.00")
     print ("3. Film 3      : 14.00")
+    film_1 = int (input("Silahkan Pilih Film (Silahkan Ketik Nomor): "))
+    if film_1 == 1:
+        spec = 'Film 1'
+        jam = '10:00'
+        harga = 50000
+    elif film_1 == 2:
+        spec = 'Film 2'
+        jam = '12:00'
+        harga = 50000
+    elif film_1 == 3:
+        spec = 'Film 3'
+        jam = '14:00'
+        harga = 50000
+    else:
+        while True:
+            print ("**********Film Tidak Tersedia, Silahkan Pilih Lagi!**********")
+            film_1 = int (input("Silahkan Pilih Film(Silahkan Ketik Nomor): "))
+            if film_1 == 1 or film_1 == 2 or film_1 == 3:
+                if film_1 == 1:
+                    spec = 'Film 1'
+                    jam = '10:00'
+                    harga = 50000
+                elif film_1 == 2:
+                    spec = 'Film 2'
+                    jam = '12:00'
+                    harga = 50000
+                elif film_1 == 3:
+                    spec = 'Film 3'
+                    jam = '14:00'
+                    harga = 50000
+                break
 elif teater == 2:
-    print ("4. Film 4      : 11.00")
-    print ("5. Film 5      : 13.00")
-    print ("6. Film 6      : 15.00")
+    print ("1. Film 4      : 11.00")
+    print ("2. Film 5      : 13.00")
+    print ("3. Film 6      : 15.00")
+    film_2 = int (input("Silahkan Pilih Film (Silahkan Ketik Nomor)"))
+    if film_2 == 1:
+        spec = 'Film 4'
+        jam = '11:00'
+        harga = 50000
+    elif film_2 == 2:
+        spec = 'Film 5'
+        jam = '13:00'
+        harga = 50000
+    elif film_2 == 3:
+        spec = 'Film 6'
+        jam = '15:00'
+        harga = 50000
+    else:
+        while True:
+            print ("**********Film Tidak Tersedia, Silahkan Pilih Lagi!**********")
+            film_2 = int (input("Silahkan Pilih Film(Silahkan Ketik Nomor): "))
+            if film_2 == 1 or film_2 == 2 or film_2 == film_2 == 3:
+                if film_2 == 1:
+                    spec = 'Film 4'
+                    jam = '11:00'
+                    harga = 50000
+                elif film_2 == 2:
+                    spec = 'Film 5'
+                    jam = '13:00'
+                    harga = 50000
+                elif film_2 == 3:
+                    spec = 'Film 6'
+                    jam = '15:00'
+                    harga = 50000
+                break
 else:
     while True:
         print ("**********Teater Tidak Tersedia, Silahkan Pilih Lagi!**********")
@@ -33,66 +95,74 @@ else:
                 print ("1. Film 1      : 10.00")
                 print ("2. Film 2      : 12.00")
                 print ("3. Film 3      : 14.00")
+                film_1 = int (input("Silahkan Pilih Film (Silahkan Ketik Nomor): "))
+                if film_1 == 1:
+                    spec = 'Film 1'
+                    jam = '10:00'
+                    harga = 50000
+                elif film_1 == 2:
+                    spec = 'Film 2'
+                    jam = '12:00'
+                    harga = 50000
+                elif film_1 == 3:
+                    spec = 'Film 3'
+                    jam = '14:00'
+                    harga = 50000
+                else:
+                    while True:
+                        print ("**********Film Tidak Tersedia, Silahkan Pilih Lagi!**********")
+                        film_1 = int (input("Silahkan Pilih Film(Silahkan Ketik Nomor): "))
+                        if film_1 == 1 or film_1 == 2 or film_1 == 3:
+                            if film_1 == 1:
+                                spec = 'Film 1'
+                                jam = '10:00'
+                                harga = 50000
+                            elif film_1 == 2:
+                                spec = 'Film 2'
+                                jam = '12:00'
+                                harga = 50000
+                            elif film_1 == 3:
+                                spec = 'Film 3'
+                                jam = '14:00'
+                                harga = 50000
+                            break
             elif teater == 2:
-                print ("4. Film 4      : 11.00")
-                print ("5. Film 5      : 13.00")
-                print ("6. Film 6      : 15.00")
+                print ("1. Film 4      : 11.00")
+                print ("2. Film 5      : 13.00")
+                print ("3. Film 6      : 15.00")
+                film_2 = int (input("Silahkan Pilih Film (Silahkan Ketik Nomor): "))
+                if film_2 == 1:
+                    spec = 'Film 4'
+                    jam = '11:00'
+                    harga = 50000
+                elif film_2 == 2:
+                    spec = 'Film 5'
+                    jam = '13:00'
+                    harga = 50000
+                elif film_2 == 3:
+                    spec = 'Film 6'
+                    jam = '15:00'
+                    harga = 50000
+                else:
+                    while True:
+                        print ("**********Film Tidak Tersedia, Silahkan Pilih Lagi!**********")
+                        film_2 = int (input("Silahkan Pilih Film(Silahkan Ketik Nomor): "))
+                        if film_2 == 1 or film_2 == 2 or film_2 == 3:
+                            if film_2 == 1:
+                                spec = 'Film 4'
+                                jam = '11:00'
+                                harga = 50000
+                            elif film_2 == 2:
+                                spec = 'Film 5'
+                                jam = '13:00'
+                                harga = 50000
+                            elif film_2 == 3:
+                                spec = 'Film 6'
+                                jam = '15:00'
+                                harga = 50000
+                            break
             break
-film = int (input("Silahkan Pilih Film (Silahkan Ketik Nomor)"))
-if film == 1:
-    spec = 'Film 1'
-    jam = '10:00'
-    harga = 50000
-elif film == 2:
-    spec = 'Film 2'
-    jam = '12:00'
-    harga = 50000
-elif film == 3:
-    spec = 'Film 3'
-    jam = '14:00'
-    harga = 50000
-elif film == 4:
-    spec = 'Film 4'
-    jam = '11:00'
-    harga = 50000
-elif film == 5:
-    spec = 'Film 5'
-    jam = '13:00'
-    harga = 50000
-elif film == 6:
-    spec = 'Film 6'
-    jam = '15:00'
-    harga = 50000
-else:
-    while True:
-        print ("**********Film Tidak Tersedia, Silahkan Pilih Lagi!**********")
-        film = int (input("Silahkan Pilih Film(Silahkan Ketik Nomor): "))
-        if film == 1 or film == 2 or film == 3 or film == 4 or film == 5 or 6:
-            if film == 1:
-                spec = 'Film 1'
-                jam = '10:00'
-                harga = 50000
-            elif film == 2:
-                spec = 'Film 2'
-                jam = '12:00'
-                harga = 50000
-            elif film == 3:
-                spec = 'Film 3'
-                jam = '14:00'
-                harga = 50000
-            elif film == 4:
-                spec = 'Film 4'
-                jam = '11:00'
-                harga = 50000
-            elif film == 5:
-                spec = 'Film 5'
-                jam = '13:00'
-                harga = 50000
-            elif film == 6:
-                spec = 'Film 6'
-                jam = '15:00'
-                harga = 50000
-            break
+
 total = int (input("Masukkan Jumlah Tiket Pembelian :"))
 print("\nNota Pembelian Tiket")
 print("```````````````````````")
